21d379ec3a14f69ba4b37412b27f98f1961b59db
Falsher <ivan.zzaharov@gmail.com>
2021-04-13 23:27:30 +0300

    git added home8

--- js/task-08.js
+++ js/task-08.js
@@ -3,31 +3,30 @@ const render = document.querySelector('button[data-action="render"]')
 const destroy = document.querySelector('button[data-action="destroy"]')
 const inputScreenAmountDiv = document.querySelector('input')
 
-// const newDiv = (event) => {
-//   const number = event.currentTarget.value
-//   let divv = document.createElement('div')
-//   divv.setAttribute('width', '30')
-//   divv.setAttribute('height', '30')
-
-//   const array = []
-
-//   for(let i = 0; i < number; i+=1){
-//   array.unshift(divv)
-//   }
-
-//   console.log(array)
-
-//   array.forEach(el => boxCreateDiv.appendChild(el))
-
-//   console.log(boxCreateDiv)
-//   }
-//   inputScreenAmountDiv.addEventListener('input', newDiv)
-
-
+let divv
+let number = 0
+const newDiv = (event) => {
+ number  = event.currentTarget.value
+  }
+inputScreenAmountDiv.addEventListener('input', newDiv)
 
 const rendering = () => {
-
-console.dir(inputScreenAmountDiv)
-
+  console.log(number)
+  for(let i = 0; i < number; i++){
+    divv = document.createElement('div')
+    divv.setAttribute('width', '30')
+    divv.setAttribute('height', '30')
+    console.log(boxCreateDiv.appendChild(divv))
+  }
 }
 render.addEventListener('click', rendering)
+
+const deleteDiv = () => {
+  for(let i = 0; i < number; i++){
+    boxCreateDiv.removeChild(divv)
+   console.log(boxCreateDiv)
+  }
+
+}
+
+destroy.addEventListener('click', deleteDiv)
