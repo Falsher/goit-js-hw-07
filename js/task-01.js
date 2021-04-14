bd635de8ae40e1a19b6dd0ca282eca8a8aac54e8
Falsher <ivan.zzaharov@gmail.com>
2021-04-14 19:33:38 +0300

    git added hw1

--- js/task-01.js
+++ js/task-01.js
@@ -1,17 +1,9 @@
 
 const categories = document.querySelectorAll('#categories li.item');
-const list1 = categories[0];
-const list2 = categories[1];
-const list3 = categories[2];
-console.log(`Категория: ${list1.querySelector('h2').textContent}`);
+categories.forEach(el => {
+
+console.log(`Категория: ${el.querySelector('h2').textContent}`)
 console.log(
-  `Количество элементов: ${list1.querySelector('ul').children.length}`,
-);
-console.log(`Категория: ${list2.querySelector('h2').textContent}`);
-console.log(
-  `Количество элементов: ${list2.querySelector('ul').children.length}`,
-);
-console.log(`Категория: ${list3.querySelector('h2').textContent}`);
-console.log(
-  `Количество элементов: ${list3.querySelector('ul').children.length}`,
-);
+  `Количество элементов: ${el.querySelector('ul').children.length}`,
+   );
+})
