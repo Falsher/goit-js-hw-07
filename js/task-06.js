293e504bec60140aef5289478ad4413108b3c3ce
Falsher <ivan.zzaharov@gmaol.com>
2021-04-14 15:41:15 +0300

    git added 6hw

--- js/task-06.js
+++ js/task-06.js
@@ -1,11 +1,17 @@
 const countString = document.querySelector('input')
+const lengthString = countString.dataset.length
 function onInputChangeName(event){
   console.log(event.currentTarget.value)
   countString.textContent = event.currentTarget.value
-  if(countString.textContent.length > 6){
-  
-    countString.classList.add('invalid')
+  if(countString.textContent.length != lengthString){
 
-  }countString.classList.add('valid')
+    countString.classList.add('invalid')
+  }else{
+    countString.classList.remove('invalid')
+    countString.classList.add('valid')
+  }
+
+
+
   }
   countString.addEventListener('input', onInputChangeName)
